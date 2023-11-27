43e51870721c3e8963e364fafaf0a0e8dfb302ce
Resul Quluzade <quluzade.resul13@mail.ru>
2023-11-28 01:52:13 +0400

    added ordering to book list view

--- api/library/views.py
+++ api/library/views.py
@@ -1,4 +1,5 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.filters import OrderingFilter
 from .models import Book
 from .serializers import BookSerializer
 
@@ -9,7 +10,8 @@ class BookAPIViewMixin:
 
 
 class BookListCreateAPIView(BookAPIViewMixin, ListCreateAPIView):
-    pass
+    filter_backends = [OrderingFilter]
+    ordering_fields = ["name"]
 
 
 class BookRetrieveUpdateDestroyAPIView(BookAPIViewMixin, RetrieveUpdateDestroyAPIView):
